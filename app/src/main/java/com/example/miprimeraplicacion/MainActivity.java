d7c4658c4bfb9d4ce26ac1e9cda71e275c5b2798
cesia-arevalo <ustr027024@ugb.edu.sv>
2025-02-04 10:24:09 -0600

    Tarea calculadora Basica

--- app/src/main/java/com/example/miprimeraplicacion/MainActivity.java
+++ app/src/main/java/com/example/miprimeraplicacion/MainActivity.java
@@ -1,14 +1,15 @@
-package com.example.miprimeraplicacion;
-
-
+package com.example.miPrimerAplicacion;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.TabHost;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,47 +17,76 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.miPrimerAplicacion.R;
+
+import org.w3c.dom.Text;
 public class MainActivity extends AppCompatActivity {
-    Button btn;
-    TextView tempVal;
-    RadioGroup rgb;
-    RadioButton opt;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Button btn;
+        TextView tempVal;
+        RadioGroup rgo;
+        RadioButton opt;
 
-        btn = findViewById(R.id.btnCalcular);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tempVal = findViewById(R.id.txtNum1);
-                double num1 = Double.parseDouble(tempVal.getText().toString());
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
 
-                tempVal = findViewById(R.id.txtNum2);
-                double num2 = Double.parseDouble(tempVal.getText().toString());
+            btn = findViewById(R.id.btnCalcular);
+            btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    tempVal = findViewById(R.id.txtNum1);
+                    double num1 = Double.parseDouble(tempVal.getText().toString());
 
-                double respuesta = 0.0;
+                    tempVal= findViewById(R.id.txtNum2);
+                    double num2 = Double.parseDouble(tempVal.getText().toString());
 
-                opt = findViewById(R.id.optSuma);
-                if (opt.isChecked()) {
-                    respuesta = num1 + num2;
+                    double respuesta = 0.0;
+
+                    opt = findViewById(R.id.optSuma);
+                    if (opt.isChecked()) {
+                        respuesta = num1 + num2;
+                    }
+                    opt = findViewById(R.id.optResta);
+                    if (opt.isChecked()) {
+                        respuesta = num1 - num2;
+                    }
+                    opt = findViewById(R.id.optMultiplicacion);
+                    if (opt.isChecked()) {
+                        respuesta = num1 * num2;
+                    }
+                    opt = findViewById(R.id.optDivision);
+                    if (opt.isChecked()) {
+                        respuesta = num1 / num2;
+                    }
+                    opt = findViewById(R.id.optExponenciacion);
+                    if (opt.isChecked()) {
+                        respuesta = Math.pow(num1, num2);
+                    }
+                    opt = findViewById(R.id.optPorcentaje);
+                    if (opt.isChecked()) {
+                        respuesta = (num1 * num2) / 100;
+                    }
+                    opt = findViewById(R.id.optRaizCuarta);
+                    if (opt.isChecked()) {
+                        respuesta = Math.pow(num1, 1.0 / 4);
+                    }
+                    opt = findViewById(R.id.optFactorial);
+                    if (opt.isChecked()) {
+                        respuesta = factorial((int) num1);
+                    }
+
+                    tempVal= findViewById(R.id.lblRespuesta);
+                    tempVal.setText("Respuesta: " + respuesta);
                 }
-                opt = findViewById(R.id.optResta);
-                if (opt.isChecked()) {
-                    respuesta = num1 - num2;
-                }
-                opt = findViewById(R.id.optMultiplicacion);
-                if (opt.isChecked()) {
-                    respuesta = num1 * num2;
-                }
-                opt = findViewById(R.id.optDivision);
-                if (opt.isChecked()) {
-                    respuesta = num1 / num2;
-                }
-                tempVal = findViewById(R.id.lblRespuesta);
-                tempVal.setText("Respuesta: "+ respuesta);
-            }
-        });
+            });
+        }
+
+        private int factorial(int num) {
+            if (num <= 1) return 1;
+            return num * factorial(num - 1);
+        }
     }
-}
+
+
+
