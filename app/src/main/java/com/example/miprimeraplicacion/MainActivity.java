6f06f19a1eaf8abb5bb5cf35508de3c285a861a1
cesia-arevalo <ustr027024@ugb.edu.sv>
2025-02-19 19:57:40 -0600

    Creacion de la rama sensor de luz

--- app/src/main/java/com/example/miprimeraplicacion/MainActivity.java
+++ app/src/main/java/com/example/miprimeraplicacion/MainActivity.java
@@ -10,6 +10,10 @@ import android.widget.TabHost;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     TextView tempVal;
     Spinner spn;
     conversores objConversores = new conversores();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,52 +30,30 @@ public class MainActivity extends AppCompatActivity {
 
         tbh = findViewById(R.id.tbhConversor);
         tbh.setup();
+
         tbh.addTab(tbh.newTabSpec("Monedas").setContent(R.id.tabMonedas).setIndicator("MONEDAS", null));
         tbh.addTab(tbh.newTabSpec("Longitud").setContent(R.id.tabLongitud).setIndicator("LONGITUD", null));
         tbh.addTab(tbh.newTabSpec("Tiempo").setContent(R.id.tabTiempo).setIndicator("TIEMPO", null));
         tbh.addTab(tbh.newTabSpec("Almacenamiento").setContent(R.id.tabAlmacenamiento).setIndicator("ALMACENAMIENTO", null));
+
         btn = findViewById(R.id.btnCalcular);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                tempVal = findViewById(R.id.txtNum1);
-                double num1 = Double.parseDouble(tempVal.getText().toString());
                 int opcion = tbh.getCurrentTab();
+
                 spn = findViewById(R.id.spnDeMonedas);
                 int de = spn.getSelectedItemPosition();
 
-                tempVal = findViewById(R.id.txtNum2);
-                double num2 = Double.parseDouble(tempVal.getText().toString());
                 spn = findViewById(R.id.spnAMonedas);
                 int a = spn.getSelectedItemPosition();
+
                 tempVal = findViewById(R.id.txtCantidad);
                 double cantidad = Double.parseDouble(tempVal.getText().toString());
 
-                double respuesta = 0.0;
-                String msg = "";
-                spn = findViewById(R.id.spnOpciones);
-                switch (spn.getSelectedItemPosition()){
-                    case 0:
-                        respuesta = num1 + num2;
-                        msg = "La suma es: "+ respuesta;
-                        break;
-                    case 1:
-                        respuesta = num1 - num2;
-                        msg = "La resta es: "+ respuesta;
-                        break;
-                    case 2:
-                        respuesta = num1 * num2;
-                        msg = "La multiplicacion es: "+ respuesta;
-                        break;
-                    case 3:
-                        respuesta = num1 / num2;
-                        msg = "La division: "+ respuesta;
-                        break;
-                }
                 tempVal = findViewById(R.id.lblRespuesta);
                 double respuesta = objConversores.convertir(opcion, de, a, cantidad);
                 tempVal.setText("Respuesta: "+ respuesta);
-                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_LONG).show();
             }
         });
     }
@@ -85,3 +68,4 @@ class conversores{
     public double convertir(int opcion, int de, int a, double cantidad){
         return valores[opcion][a] / valores[opcion][de] * cantidad;
     }
+}
