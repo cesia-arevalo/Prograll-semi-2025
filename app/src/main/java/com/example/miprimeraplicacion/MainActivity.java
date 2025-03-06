1348657c42ee7d3c2984c96e363bb25ce4baeedd
cesia-arevalo <ustr027024@ugb.edu.sv>
2025-03-06 15:45:26 -0600

    Se programa el poder guardar registros de amigos y listar los amigos

--- app/src/main/java/com/example/miprimeraplicacion/MainActivity.java
+++ app/src/main/java/com/example/miprimeraplicacion/MainActivity.java
@@ -3,21 +3,19 @@ package com.example.miprimeraplicacion;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.Spinner;
-import android.widget.TabHost;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
+    TextView tempVal;
+    DB db;
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    }
+    private void guardarAmigo() {
+    }
 
-}
 }
