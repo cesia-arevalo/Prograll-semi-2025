89ee05c29ec648f25268bc425c8ae44d2cf4200e
cesia-arevalo <ustr027024@ugb.edu.sv>
2025-05-10 23:21:35 +0000

    comenzamos a programar el proyecto con firebase

--- app/src/main/java/com/example/miprimeraplicacion/DB.java
+++ app/src/main/java/com/example/miprimeraplicacion/DB.java
@@ -5,44 +5,10 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import android.content.ContentValues;
-import android.content.Context;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-
-import android.content.ContentValues;
-import android.content.Context;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class DB extends SQLiteOpenHelper {
-    public static final String COL_ID_BEBIDA = "idBebida";
-    public static final String COL_CODIGO = "codigo";
-    public static final String COL_DESCRIPCION = "descripcion";
-    public static final String COL_MARCA = "marca";
-    public static final String COL_PRESENTACION = "presentacion";
-    public static final String COL_PRECIO = "precio";
-    public static final String COL_URL_FOTOS = "urlFotos";
-    private static final String DATABASE_NAME = "bebidas";
+    private static final String DATABASE_NAME = "amigos";
     private static final int DATABASE_VERSION = 1;
-    public static final String TABLA_BEBIDAS = "bebidas";
-    private static final String SQLdb = "CREATE TABLE " + TABLA_BEBIDAS + " (" +
-            COL_ID_BEBIDA + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-            COL_CODIGO + " TEXT, " +
-            COL_DESCRIPCION + " TEXT, " +
-            COL_MARCA + " TEXT, " +
-            COL_PRESENTACION + " TEXT, " +
-            COL_PRECIO + " REAL, " +
-            COL_URL_FOTOS + " TEXT)";
-
+    private static final String SQLdb = "CREATE TABLE amigos (idAmigo TEXT, nombre TEXT, direccion TEXT, telefono TEXT, email TEXT, dui TEXT, urlFoto TEXT, miToken TEXT)";
     public DB(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
@@ -51,87 +17,34 @@ public class DB extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(SQLdb);
     }
-
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        //Actualizar la estrucutra de la base de datos si es necesario
     }
-
-    public String administrar_bebidas(String accion, String[] datos) {
-        try {
+    public String administrar_amigos(String accion, String[] datos) {
+        try{
             SQLiteDatabase db = getWritableDatabase();
-            String mensaje = "ok";
-            ContentValues values = new ContentValues();
+            String mensaje = "ok", sql = "";
             switch (accion) {
                 case "nuevo":
-                    values.put(COL_CODIGO, datos[0]);
-                    values.put(COL_DESCRIPCION, datos[1]);
-                    values.put(COL_MARCA, datos[2]);
-                    values.put(COL_PRESENTACION, datos[3]);
-                    values.put(COL_PRECIO, Double.parseDouble(datos[4])); // Convertir precio a Double
-                    values.put(COL_URL_FOTOS, datos[5]); // Almacenar rutas separadas por un delimitador
-                    long newRowId = db.insert(TABLA_BEBIDAS, null, values);
-                    if (newRowId == -1) {
-                        mensaje = "Error al insertar la bebida.";
-                    }
+                    sql = "INSERT INTO amigos (nombre, direccion, telefono, email, dui, urlFoto, miToken) VALUES ('"+ datos[1] +"', '" + datos[2] + "', '" + datos[3] + "', '" + datos[4] + "', '" + datos[5] + "', '" + datos[6] + "', '" + datos[7] + "')";
                     break;
                 case "modificar":
-                    values.put(COL_CODIGO, datos[1]);
-                    values.put(COL_DESCRIPCION, datos[2]);
-                    values.put(COL_MARCA, datos[3]);
-                    values.put(COL_PRESENTACION, datos[4]);
-                    values.put(COL_PRECIO, Double.parseDouble(datos[5])); // Convertir precio a Double
-                    values.put(COL_URL_FOTOS, datos[6]); // Almacenar rutas separadas por un delimitador
-                    int rowsAffected = db.update(TABLA_BEBIDAS, values, COL_ID_BEBIDA + " = ?", new String[]{datos[0]});
-                    if (rowsAffected == 0) {
-                        mensaje = "No se encontró la bebida para modificar.";
-                    }
+                    sql = "UPDATE amigos SET nombre = '" + datos[1] + "', direccion = '" + datos[2] + "', telefono = '" + datos[3] + "', email = '" + datos[4] + "', dui = '" + datos[5] + "', urlFoto = '" + datos[6] + "' WHERE idAmigo = '" + datos[0] + "'";
                     break;
                 case "eliminar":
-                    int rowsDeleted = db.delete(TABLA_BEBIDAS, COL_ID_BEBIDA + " = ?", new String[]{datos[0]});
-                    if (rowsDeleted == 0) {
-                        mensaje = "No se encontró la bebida para eliminar.";
-                    }
+                    sql = "DELETE FROM amigos WHERE idAmigo = '" + datos[0] + "'";
                     break;
             }
+            db.execSQL(sql);
             db.close();
             return mensaje;
         } catch (Exception e) {
             return e.getMessage();
         }
     }
-
-    public Cursor lista_bebidas() {
+    public Cursor lista_amigos() {
         SQLiteDatabase db = getReadableDatabase();
-        return db.rawQuery("SELECT * FROM " + TABLA_BEBIDAS, null);
-    }
-
-    // Función para obtener una bebida por su ID (necesaria para la edición)
-    public Bebidas obtener_bebida(String id) {
-        SQLiteDatabase db = getReadableDatabase();
-        Cursor cursor = db.query(
-                TABLA_BEBIDAS,
-                new String[]{COL_ID_BEBIDA, COL_CODIGO, COL_DESCRIPCION, COL_MARCA, COL_PRESENTACION, COL_PRECIO, COL_URL_FOTOS},
-                COL_ID_BEBIDA + "=?",
-                new String[]{id},
-                null,
-                null,
-                null,
-                null
-        );
-        Bebidas bebida = null;
-        if (cursor.moveToFirst()) {
-            bebida = new Bebidas(
-                    cursor.getString(cursor.getColumnIndexOrThrow(COL_ID_BEBIDA)),
-                    cursor.getString(cursor.getColumnIndexOrThrow(COL_CODIGO)),
-                    cursor.getString(cursor.getColumnIndexOrThrow(COL_DESCRIPCION)),
-                    cursor.getString(cursor.getColumnIndexOrThrow(COL_MARCA)),
-                    cursor.getString(cursor.getColumnIndexOrThrow(COL_PRESENTACION)),
-                    String.valueOf(cursor.getDouble(cursor.getColumnIndexOrThrow(COL_PRECIO))), // Obtener como Double y luego a String
-                    new ArrayList<>(Arrays.asList(cursor.getString(cursor.getColumnIndexOrThrow(COL_URL_FOTOS)).split(";")))
-            );
-            cursor.close();
-        }
-        db.close();
-        return bebida;
+        return db.rawQuery("SELECT * FROM amigos", null);
     }
 }
--- app/src/main/java/com/example/miprimeraplicacion/MainActivity.java
+++ app/src/main/java/com/example/miprimeraplicacion/MainActivity.java
@@ -1,213 +1,200 @@
 package com.example.miprimeraplicacion;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.text.TextUtils;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     FloatingActionButton fab;
     Button btn;
     TextView tempVal;
-    DB db;
-    String accion = "nuevo", idBebida = "", id = "", rev = "";
+    String accion = "nuevo", idAmigo = "", id="", rev="";
     ImageView img;
-    ArrayList<String> urlFotos = new ArrayList<>();
+    String urlCompletaFoto = "";
     Intent tomarFotoIntent;
-    utilidades utls;
     detectarInternet di;
-    private static final int REQUEST_IMAGE_CAPTURE = 1;
-
+    DatabaseReference databaseReference;
+    String miToken = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        utls = new utilidades();
-        img = findViewById(R.id.imgfotoBebida);
-        db = new DB(this);
-        btn = findViewById(R.id.btnGuardarBebida);
-        btn.setOnClickListener(view -> guardarBebida());
+        obtenerToken();
+        img = findViewById(R.id.imgFotoAmigo);
+
+        btn = findViewById(R.id.btnGuardarAmigo);
+        btn.setOnClickListener(view->guardarAmigo());
+
+        fab = findViewById(R.id.fabListaAmigos);
+        fab.setOnClickListener(view->abrirVentana());
 
         mostrarDatos();
         tomarFoto();
-
-        fab = findViewById(R.id.fabListaBebidas); // Asegúrate de tener este ID en tu layout
-        if (fab != null) {
-            fab.setOnClickListener(view -> abrirVentanaListaBebidas());
+    }
+    private void obtenerToken(){
+        try{
+            FirebaseMessaging.getInstance().getToken().addOnCompleteListener(tarea->{
+                if(!tarea.isSuccessful()){
+                    mostrarMsg("Error al obtener token: "+tarea.getException().getMessage());
+                }else{
+                    miToken = tarea.getResult();
+                }
+            });
+        }catch (Exception e){
+            mostrarMsg("Error al obtener token: "+e.getMessage());
         }
     }
-
-    private void mostrarDatos() {
+    private void mostrarDatos(){
         try {
             Bundle parametros = getIntent().getExtras();
-            if (parametros != null && parametros.containsKey("accion")) {
-                accion = parametros.getString("accion");
-                mostrarMsg("La accion es: " + accion);
-                if (accion.equals("modificar")) {
-                    JSONObject datos = new JSONObject(parametros.getString("bebidas"));
-                    idBebida = datos.getString("idBebida");
+            accion = parametros.getString("accion");
+            if (accion.equals("modificar")) {
+                JSONObject datos = new JSONObject(parametros.getString("amigos"));
+                id = datos.getString("_id");
+                rev = datos.getString("_rev");
+                idAmigo = datos.getString("idAmigo");
 
-                    tempVal = findViewById(R.id.txtCodigo);
-                    tempVal.setText(datos.getString("codigo"));
+                tempVal = findViewById(R.id.txtNombre);
+                tempVal.setText(datos.getString("nombre"));
 
-                    tempVal = findViewById(R.id.txtDescripcion);
-                    tempVal.setText(datos.getString("descripcion"));
+                tempVal = findViewById(R.id.txtDireccion);
+                tempVal.setText(datos.getString("direccion"));
 
-                    tempVal = findViewById(R.id.txtMarca);
-                    tempVal.setText(datos.getString("marca"));
+                tempVal = findViewById(R.id.txtTelefono);
+                tempVal.setText(datos.getString("telefono"));
 
-                    tempVal = findViewById(R.id.txtPresentacion);
-                    tempVal.setText(datos.getString("presentacion"));
+                tempVal = findViewById(R.id.txtEmail);
+                tempVal.setText(datos.getString("email"));
 
-                    tempVal = findViewById(R.id.txtPrecio);
-                    tempVal.setText(datos.getString("precio"));
+                tempVal = findViewById(R.id.txtDui);
+                tempVal.setText(datos.getString("dui"));
 
-                    String fotosConcat = datos.getString("foto");
-                    String[] rutas = fotosConcat.split("\\|");
-                    urlFotos = new ArrayList<>(Arrays.asList(rutas));
-                    if (!urlFotos.isEmpty() && new File(urlFotos.get(urlFotos.size() - 1)).exists()) {
-                        Bitmap bitmap = BitmapFactory.decodeFile(urlFotos.get(urlFotos.size() - 1));
-                        img.setImageBitmap(bitmap);
-                    }
+                urlCompletaFoto = datos.getString("urlFoto");
+                img.setImageURI(Uri.parse(urlCompletaFoto));
+            }else {
+                //idAmigo = ;
+            }
+        }catch (Exception e){
+            mostrarMsg("Error: "+e.getMessage());
+        }
+    }
+    private void tomarFoto(){
+        img.setOnClickListener(view->{
+            tomarFotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            File fotoAmigo = null;
+            try{
+                fotoAmigo = crearImagenAmigo();
+                if( fotoAmigo!=null ){
+                    Uri uriFotoAimgo = FileProvider.getUriForFile(MainActivity.this,
+                            "com.ugb.miprimeraaplicacion.fileprovider", fotoAmigo);
+                    tomarFotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, uriFotoAimgo);
+                    startActivityForResult(tomarFotoIntent, 1);
+                }else{
+                    mostrarMsg("Nose pudo crear la imagen.");
                 }
-            } else {
-                mostrarMsg("No se recibió la acción.");
+            }catch (Exception e){
+                mostrarMsg("Error: "+e.getMessage());
             }
-        } catch (Exception e) {
-            mostrarMsg("Error al mostrar datos: " + e.getMessage());
-        }
-    }
-
-    private void tomarFoto() {
-        img.setOnClickListener(view -> checkCameraPermission());
-    }
-
-    private void checkCameraPermission() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_IMAGE_CAPTURE);
-        } else {
-            dispatchTakePictureIntent();
-        }
-    }
-
-    private void dispatchTakePictureIntent() {
-        tomarFotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        File fotoBebida = null;
-        try {
-            fotoBebida = crearImagenBebida();
-            if (fotoBebida != null) {
-                Uri uriFotobebida = FileProvider.getUriForFile(MainActivity.this,
-                        getApplicationContext().getPackageName() + ".fileprovider", fotoBebida);
-                tomarFotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, uriFotobebida);
-                startActivityForResult(tomarFotoIntent, REQUEST_IMAGE_CAPTURE);
-            } else {
-                mostrarMsg("No se pudo crear el archivo de imagen.");
-            }
-        } catch (Exception e) {
-            mostrarMsg("Error al crear archivo de imagen: " + e.getMessage());
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == REQUEST_IMAGE_CAPTURE) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                dispatchTakePictureIntent();
-            } else {
-                mostrarMsg("Permiso de cámara denegado.");
-            }
-        }
+        });
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        try {
-            if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-                if (!urlFotos.isEmpty() && new File(urlFotos.get(urlFotos.size() - 1)).exists()) {
-                    img.setImageURI(Uri.parse(urlFotos.get(urlFotos.size() - 1)));
-                }
-            } else {
-                mostrarMsg("No se tomó la foto o se canceló.");
+        try{
+            if( requestCode==1 && resultCode==RESULT_OK ){
+                //Bitmap imagenBitmap = BitmapFactory.decodeFile(urlCompletaFoto);
+                img.setImageURI(Uri.parse(urlCompletaFoto));
+            }else{
+                mostrarMsg("No se tomo la foto.");
             }
-        } catch (Exception e) {
-            mostrarMsg("Error al procesar resultado de la cámara: " + e.getMessage());
+        }catch (Exception e){
+            mostrarMsg("Error: "+e.getMessage());
         }
     }
 
-    private File crearImagenBebida() throws Exception {
+    private File crearImagenAmigo() throws Exception{
         String fechaHoraMs = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date()),
-                fileName = "imagen_" + fechaHoraMs + "_";
-        File dirAlmacenamiento = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        if (!dirAlmacenamiento.exists()) {
-            dirAlmacenamiento.mkdirs();
+                fileName = "imagen_"+ fechaHoraMs+"_";
+        File dirAlmacenamiento = getExternalFilesDir(Environment.DIRECTORY_DCIM);
+        if( dirAlmacenamiento.exists()==false ){
+            dirAlmacenamiento.mkdir();
         }
         File image = File.createTempFile(fileName, ".jpg", dirAlmacenamiento);
-        urlFotos.add(image.getAbsolutePath());
+        urlCompletaFoto = image.getAbsolutePath();
         return image;
     }
-
-    private void mostrarMsg(String msg) {
+    private void mostrarMsg(String msg){
         Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
     }
-
-    private void abrirVentanaListaBebidas() {
-        Intent intent = new Intent(this, lista_bebidas.class);
+    private void abrirVentana(){
+        Intent intent = new Intent(this, lista_amigos.class);
         startActivity(intent);
     }
+    private void guardarAmigo() {
+        try {
+            tempVal = findViewById(R.id.txtNombre);
+            String nombre = tempVal.getText().toString();
 
-    private void guardarBebida() {
-        EditText etCodigo = findViewById(R.id.txtCodigo);
-        EditText etDescripcion = findViewById(R.id.txtDescripcion);
-        EditText etMarca = findViewById(R.id.txtMarca);
-        EditText etPresentacion = findViewById(R.id.txtPresentacion);
-        EditText etPrecio = findViewById(R.id.txtPrecio);
+            tempVal = findViewById(R.id.txtDireccion);
+            String direccion = tempVal.getText().toString();
 
-        String codigo = etCodigo.getText().toString().trim();
-        String descripcion = etDescripcion.getText().toString().trim();
-        String marca = etMarca.getText().toString().trim();
-        String presentacion = etPresentacion.getText().toString().trim();
-        String precio = etPrecio.getText().toString().trim();
+            tempVal = findViewById(R.id.txtTelefono);
+            String telefono = tempVal.getText().toString();
 
-        String urlsFotosConcat = TextUtils.join("|", urlFotos);
-        String[] datos = {idBebida, codigo, descripcion, marca, presentacion, precio, urlsFotosConcat};
-        Bundle parametros = getIntent().getExtras();
-        if (parametros != null && parametros.containsKey("accion")) {
-            accion = parametros.getString("accion");
+            tempVal = findViewById(R.id.txtEmail);
+            String email = tempVal.getText().toString();
+
+            tempVal = findViewById(R.id.txtDui);
+            String dui = tempVal.getText().toString();
+
+            databaseReference = FirebaseDatabase.getInstance().getReference("amigos");
+            String key = databaseReference.push().getKey();
+
+            if( miToken.equals("") || miToken==null ){
+                obtenerToken();
+            }
+            amigos amigo = new amigos(idAmigo, nombre, direccion, telefono, email, dui, urlCompletaFoto, miToken);
+            if( key!= null ){
+                databaseReference.child(key).setValue(amigo).addOnSuccessListener(success->{
+                    mostrarMsg("Registro guardado con exito.");
+                    abrirVentana();
+                }).addOnFailureListener(failure->{
+                    mostrarMsg("Error: "+failure.getMessage());
+                });
+            } else {
+                mostrarMsg("Error al guardar el registro.");
+            }
+        }catch (Exception e){
+            mostrarMsg("Error: "+e.getMessage());
         }
-
-        db.administrar_bebidas(accion, datos);
-        Toast.makeText(getApplicationContext(), "Registro guardado con éxito.", Toast.LENGTH_LONG).show();
-        abrirVentanaListaBebidas();
     }
 }
