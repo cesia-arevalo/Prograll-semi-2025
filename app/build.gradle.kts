5bd1fd61d148966fa8186ae2368872af1c0702a2
cesia-arevalo <ustr027024@ugb.edu.sv>
2025-06-01 23:51:45 +0000

    Continuamos con el chats de amigos

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,14 +1,15 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("com.google.gms.google-services")
 }
 
 android {
-    namespace = "com.example.miprimeraplicacion"
+    namespace = "com.ugb.miprimeraaplicacion"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.miprimeraplicacion"
-        minSdk = 22
+        applicationId = "com.ugb.miprimeraaplicacion"
+        minSdk = 23
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
@@ -37,6 +38,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.firebase.database)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
